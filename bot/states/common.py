baead7f70da1a46aecba61baae0b98c2485d5e56
teyzoo <igorborisenko676@icloud.com>
2026-08-09 06:39:22 +0300

    Update common.py

--- bot/states/common.py
+++ bot/states/common.py
@@ -2,18 +2,21 @@ from aiogram.fsm.state import (
     State,
     StatesGroup,
 )
-# =========================================================
-# COMMON / HUNTER SEARCH
-# =========================================================
+
+
+class PromoState(StatesGroup):
+
+    code = State()
+
+
 class HunterSearchState(StatesGroup):
-    length_6 = State()
+
     length_5 = State()
+    length_6 = State()
     expensive = State()
     dictionary = State()
+
+
 class HunterMaskState(StatesGroup):
+
     mask = State()
-# =========================================================
-# PROMO
-# =========================================================
-class PromoState(StatesGroup):
-    code = State()
