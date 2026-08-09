bc6da84b9f56fb07d77b5b0a918ec49a24a029cd
teyzoo <igorborisenko676@icloud.com>
2026-08-09 05:40:29 +0300

    Update __init__.py

--- services/hunter/__init__.py
+++ services/hunter/__init__.py
@@ -1,1 +1,26 @@
+from .engine import HunterEngine
 
+from .beautiful_search import (
+    BeautifulSearch,
+    BeautifulSearchConfig,
+)
+
+from .beautiful_generator import (
+    generate_candidates,
+    generate_beautiful,
+)
+
+from .beautiful_ranker import (
+    BeautifulCandidate,
+    rank_beautiful,
+)
+
+__all__ = [
+    "HunterEngine",
+    "BeautifulSearch",
+    "BeautifulSearchConfig",
+    "BeautifulCandidate",
+    "generate_candidates",
+    "generate_beautiful",
+    "rank_beautiful",
+]
