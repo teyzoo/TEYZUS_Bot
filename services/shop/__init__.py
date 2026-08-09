0a8d4d2d24e1205f832299eb3d896c747e7365df
teyzoo <igorborisenko676@icloud.com>
2026-08-09 16:13:35 +0300

    Create __init__.py

---
+++ services/shop/__init__.py
@@ -0,0 +1,11 @@
+from services.shop.service import ShopService
+from services.shop.cart import CartService
+from services.shop.favorites import FavoriteService
+from services.shop.deals import DealService
+
+__all__ = [
+    "ShopService",
+    "CartService",
+    "FavoriteService",
+    "DealService",
+]
